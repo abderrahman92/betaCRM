28489ff985e346e8e8308bfcf6147bf1033fb29c
abderrahman bensalah <bensalahabdelrrahman@gmail.com>
2022-06-21 17:36:26 +0100

    test2

--- serveur/index.js
+++ serveur/index.js
@@ -14,7 +14,7 @@ var corsOptions = {
 };
 
 app.use(express.static(path.join(__dirname + "/public")))
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 // parse requests of content-type - application/json
 app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
